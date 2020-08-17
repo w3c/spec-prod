ee220d53a8b1b18ccc53fa9e47c91ffec94082fc
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-08-18 01:19:11 +0530

    feat(build): support building ReSpec documents

---
+++ build.sh
@@ -0,0 +1,29 @@
+source_file="$INPUTS_SOURCE"
+if [ -z "$source_file" ]; then
+  case $INPUTS_TYPE in
+    bikeshed) source_file="index.bs" ;;
+    *) source_file="index.html" ;;
+  esac
+fi
+
+if [ ! -f "$source_file" ]; then
+  echo "Source file '$source_file' does not exist."
+  exit 1
+fi
+
+OUTFILE="tmp-output.html"
+
+case $INPUTS_TYPE in
+  respec)
+    echo "Converting ReSpec document '$source_file' to HTML..."
+    respec -s "$source_file" -o "$OUTFILE"
+    ;;
+  bikeshed)
+    echo "Converting Bikeshed document '$source_file' to HTML..."
+    bikeshed -h
+    ;;
+  *)
+    echo "Unknown type: $INPUTS_TYPE"
+    exit 1
+    ;;
+esac
