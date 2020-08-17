1ab66bdab5f6d5ab46fccd0424c3e6a45db22daa
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-08-18 01:52:06 +0530

    refactor(setup): prepare npm, puppeteer globally

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
+mkdir -p $HOME/.npm-global
+npm config set prefix "$HOME/.npm-global"
+export PATH=$HOME/.npm-global/bin:$PATH
+echo "::add-path::$HOME/.npm-global/bin"
+echo "::set-env name=PUPPETEER_SKIP_CHROMIUM_DOWNLOAD::1"
+echo "::set-env name=PUPPETEER_EXECUTABLE_PATH::/usr/bin/google-chrome"
+
 case $INPUTS_TYPE in
   respec)
     echo "Installing ReSpec..."
-
-    mkdir -p $HOME/.npm-global
-    npm config set prefix "$HOME/.npm-global"
-    export PATH=$HOME/.npm-global/bin:$PATH
-    echo "::add-path::$HOME/.npm-global/bin"
-
     export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
     npm install --global --silent respec
-    echo "::set-env name=PUPPETEER_EXECUTABLE_PATH::/usr/bin/google-chrome"
     ;;
   bikeshed)
     echo "Installing Bikeshed..."
