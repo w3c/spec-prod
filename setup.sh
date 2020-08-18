537ec7d7013efa96f38e8dc4e535e24d56970e77
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-08-19 00:31:23 +0530

    fix(setup): remove requirement for setup-python action

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ export PATH=$HOME/.npm-global/bin:$PATH
 echo "::add-path::$HOME/.npm-global/bin"
 echo "::set-env name=PUPPETEER_SKIP_CHROMIUM_DOWNLOAD::1"
 echo "::set-env name=PUPPETEER_EXECUTABLE_PATH::/usr/bin/google-chrome"
+export PATH=$HOME/.local/bin:$PATH
+echo "::add-path::$HOME/.local/bin"
 
 case $INPUTS_TYPE in
   respec)
