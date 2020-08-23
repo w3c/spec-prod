949c88e8f045e526703a7a3ab2a9a6f26a6b1132
Sid Vishnoi <sidvishnoi8@gmail.com>
2020-08-24 02:44:14 +0530

    refactor(build): remove code made redundant by prepare

--- build.sh
+++ build.sh
@@ -1,30 +1,13 @@
-source_file="$INPUTS_SOURCE"
-if [ -z "$source_file" ]; then
-  case $INPUTS_TYPE in
-    bikeshed) source_file="index.bs" ;;
-    *) source_file="index.html" ;;
-  esac
-fi
-
-if [ ! -f "$source_file" ]; then
-  echo "Source file '$source_file' does not exist."
-  exit 1
-fi
-
 OUTFILE="tmp-output.html"
 
 case $INPUTS_TYPE in
   respec)
-    echo "Converting ReSpec document '$source_file' to HTML..."
-    respec -s "$source_file" -o "$OUTFILE"
+    echo "Converting ReSpec document '$INPUTS_SOURCE' to HTML..."
+    respec -s "$INPUTS_SOURCE" -o "$OUTFILE"
     ;;
   bikeshed)
-    echo "Converting Bikeshed document '$source_file' to HTML..."
-    bikeshed spec "$source_file" "$OUTFILE"
-    ;;
-  *)
-    echo "Unknown type: $INPUTS_TYPE"
-    exit 1
+    echo "Converting Bikeshed document '$INPUTS_SOURCE' to HTML..."
+    bikeshed spec "$INPUTS_SOURCE" "$OUTFILE"
     ;;
 esac
 
