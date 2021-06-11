cfa5aed2d4febee3fd21803b93e7af645cf14d1d
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-06-11 16:04:41 +0530

    fix(constants): skip Puppeteer Chromium download as asked
    - always skip download in GitHub Actions runtime
    - specify PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1 to skip locally

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,14 @@ import * as path from "path";
 
 export const ACTION_DIR = path.join(__dirname, "..");
 
-export const PUPPETEER_ENV = {
-	PUPPETEER_SKIP_CHROMIUM_DOWNLOAD: "1",
-	PUPPETEER_EXECUTABLE_PATH: "/usr/bin/google-chrome",
-};
+export const PUPPETEER_ENV = (() => {
+	const skipChromeDownload =
+		!!process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD ||
+		!!process.env.GITHUB_ACTIONS;
+	if (!skipChromeDownload) return {};
+	return {
+		PUPPETEER_SKIP_CHROMIUM_DOWNLOAD: "1",
+		PUPPETEER_EXECUTABLE_PATH:
+			process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome",
+	};
+})();
