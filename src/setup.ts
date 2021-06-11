f5857f08c7324ff30be5a89dce9ecbdee81b28f5
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-06-11 15:48:37 +0530

    fix(setup): print bikeshed version  installed by pip

--- src/setup.ts
+++ src/setup.ts
@@ -31,7 +31,7 @@ export default async function main(toolchain: "respec" | "bikeshed" | string) {
 			});
 			exportVariable("PYTHONUSERBASE", PYTHONUSERBASE);
 			await sh("bikeshed update", "stream");
-			await sh("bikeshed --version", "buffer");
+			await sh("pip3 show bikeshed | grep -i version", "buffer");
 			break;
 		}
 		default: {
