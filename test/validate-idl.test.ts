1d05aef60c3e6b533aeac0721937dca9576c7629
Francois Daoust <fd@tidoust.net>
2021-09-10 10:43:26 +0200

    Lint code

--- test/validate-idl.test.ts
+++ test/validate-idl.test.ts
@@ -2,13 +2,13 @@ import main from "../src/validate-idl.js";
 import { Outputs } from "./index.test.js";
 
 export default async function validateIdl(outputs: Outputs) {
-  const { idl: shouldValidate = false } = outputs?.prepare?.validate || {};
-  if (shouldValidate === false) {
-    return;
-  }
+	const { idl: shouldValidate = false } = outputs?.prepare?.validate || {};
+	if (shouldValidate === false) {
+		return;
+	}
 
-  const { dest = process.cwd() + ".common", file = "index.html" } =
-    outputs?.build?.w3c || {};
+	const { dest = process.cwd() + ".common", file = "index.html" } =
+		outputs?.build?.w3c || {};
 
-  return await main({ dest, file });
+	return await main({ dest, file });
 }
