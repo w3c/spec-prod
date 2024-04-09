e157cd7cc1fec5f266ae55dbe3c077aa77226942
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2024-04-09 17:38:34 +0530

    fix: rename puppeteer env var flag to `PUPPETEER_SKIP_DOWNLOAD`

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,11 @@ export const ACTION_DIR = path.join(__dirname, "..");
 
 export const PUPPETEER_ENV = (() => {
 	const skipChromeDownload =
-		!!process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD ||
+		!!process.env.PUPPETEER_SKIP_DOWNLOAD ||
 		!!process.env.GITHUB_ACTIONS;
 	if (!skipChromeDownload) return {};
 	return {
-		PUPPETEER_SKIP_CHROMIUM_DOWNLOAD: "1",
+		PUPPETEER_SKIP_DOWNLOAD: "1",
 		PUPPETEER_EXECUTABLE_PATH:
 			process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome",
 	};
