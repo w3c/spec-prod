5402361c58b20eaee760ec521d3d28f83a3e5a36
Jeffrey Yasskin <jyasskin@google.com>
2025-06-18 09:05:58 -0700

    fix: remove the W3C_API_KEY requirement for VALIDATE_PUBRULES (#205)

--- src/prepare-validate.ts
+++ src/prepare-validate.ts
@@ -1,14 +1,11 @@
-import { yesOrNo, exit } from "./utils.js";
 import { Inputs } from "./prepare.js";
+import { yesOrNo } from "./utils.js";
 
 export function validation(inputs: Inputs) {
 	const input_markup = yesOrNo(inputs.VALIDATE_INPUT_MARKUP) || false;
 	const links = yesOrNo(inputs.VALIDATE_LINKS) || false;
 	const markup = yesOrNo(inputs.VALIDATE_MARKUP) || false;
 	const pubrules = yesOrNo(inputs.VALIDATE_PUBRULES) || false;
-	if (pubrules && !inputs.W3C_API_KEY) {
-		exit("The W3C_API_KEY input is required with VALIDATE_PUBRULES");
-	}
 	const webidl = yesOrNo(inputs.VALIDATE_WEBIDL) || false;
 	return { input_markup, links, markup, pubrules, webidl };
 }
